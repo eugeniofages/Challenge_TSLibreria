3204d53ad3e181f3adc411dad690d9188d5e2712
Eugenio Fages <eugeniofages@MBP-de-Eugenio.fibertel.com.ar>
2024-02-20 13:37:08 -0300

    scripts agregados

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd frontend
+
+docker build -t frontend-image .
+
+docker run -d -p 5173:5173 --name frontend-container frontend-image
+
+cd ..
+
+cd backend
+
+docker-compose up --build -d
+
---
+++ run.sh
@@ -0,0 +1,2 @@
+docker exec backend-backend-1 sh -c "cd /var/www/html && php artisan migrate --seed" 
+
